335dae3985f5a7581987735bf95e7b4439eda126
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2023-10-30 16:36:08 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0.0"
+    }
+  }
+}
+#To create KMS resource 
+resource "aws_kms_key" "tokyo_kms_key" {
+  description             = "KMS key for Tokyo"
+  deletion_window_in_days = 10
+  key_usage               = "ENCRYPT_DECRYPT"
+  enable_key_rotation     = true
+  tags = {
+    Name = "tokyo_kms_key"    
+  }
+}
+
+#To create KMS Policy 
+resource "aws_kms_key_policy" "tokyo_kms_key_policy" {
+  key_id = aws_kms_key.tokyo_kms_key.arn
+  policy = jsonencode({
+    Id = "KMS policy"
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::297200899628:root"
+        }
+
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
